d7c9afe074f863b433fa547043cb5a590d9a79a3
sheycc <shsljm2903@gmail.com>
2024-01-09 11:41:17 +0100

    Add a new content folder for docs

--- docs/portafolio-app/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-A2XZSXUL.mjs";import"./chunk-ETO5H6QW.mjs";import"./chunk-QRD5AGAP.mjs";import"./chunk-FDI5VTYX.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
